5638d0b261d5ab159eb457ce794529540a5ffd00
winsofttutorials <winsofttutorials@gmail.com>
2023-05-07 21:56:17 +0100

    sidebar logic

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var togglebtn = document.getElementById("toggle-btn");
 var subPackages = document.getElementsByClassName("sub-package");
 var subPlans = document.getElementsByClassName("sub-plans");
 var gamePlans = document.getElementsByClassName("gamesFrame");
+var stagePlans = document.getElementsByClassName("current-stage");
 
 let currentValue = 1;
 let currentValues = 1;
 let gameValues = 1;
+let stageValues = 1;
 
 function activlink(tabname) {
   if (currentValue == 1) {
@@ -55,6 +57,26 @@ function activeGame() {
   }
 }
 
+function activeStage() {
+  if (stageValues < 4) {
+    for (const stageplan of stagePlans) {
+      stageplan.classList.remove("active-stage");
+    }
+    stageValues++;
+    stagePlans[stageValues - 1].classList.add("active-stage");
+  }
+}
+
+function activebackStage() {
+  if (stageValues > 1) {
+    for (const stageplan of stagePlans) {
+      stageplan.classList.remove("active-stage");
+    }
+    stageValues--;
+    stagePlans[stageValues - 1].classList.add("active-stage");
+  }
+}
+
 function toggleMode() {
   activeTab();
   activeGame();
@@ -81,41 +103,48 @@ let back3 = document.getElementById("back3");
 next1.onclick = function () {
   form1.style.left = "600px";
   form2.style.left = "0px";
+  activeStage();
 };
 next1.addEventListener("click", next1);
 
 back1.onclick = function () {
   form2.style.left = "600px";
   form1.style.left = "0px";
+  activebackStage();
 };
 back1.addEventListener("click", next1);
 
 next2.onclick = function () {
   form2.style.left = "600px";
   form3.style.left = "0px";
+  activeStage();
 };
 next2.addEventListener("click", next1);
 
 back2.onclick = function () {
   form3.style.left = "600px";
   form2.style.left = "0px";
+  activebackStage();
 };
 back2.addEventListener("click", next1);
 
 next3.onclick = function () {
   form3.style.left = "600px";
   form4.style.left = "0px";
+  activeStage();
 };
 next3.addEventListener("click", next1);
 
 back3.onclick = function () {
   form4.style.left = "600px";
   form3.style.left = "0px";
+  activebackStage();
 };
 back3.addEventListener("click", next1);
 
 next4.onclick = function () {
   form4.style.left = "600px";
   form5.style.left = "0px";
+  activeStage();
 };
 next4.addEventListener("click", next1);
